1dd2c271ae4b2940fde884af595d69aca157cd1d
mackenziebishop <69914681+mackenziebishop@users.noreply.github.com>
2022-02-04 12:51:02 -0600

    Removed print, changed delimiter

--- 01mapper.py
+++ 01mapper.py
@@ -4,12 +4,9 @@
 f = open("Student_Marks.txt","r")  # open file, read-only
 o = open("out01.txt", "w") # open file, write
 for line in f:  
-    dataList = line.strip().split("    ") 
-    print (dataList )
-    print (len(dataList ))
-    if len(dataList) == 6:
+    dataList = line.strip().split("\t") 
+    if len(dataList) == 3:
         number_courses, time_study, marks = dataList  #assign names
-        print ("{0}\t{1}".format(number_courses, marks))
         o.write("{0}\t{1}\n".format(number_courses, marks))
 f.close()
 o.close()
